38b061bd59b04102fd1eb10ed52c9a7c7bc1af91
TheNetsky <56271887+TheNetsky@users.noreply.github.com>
2022-11-14 22:07:04 +0100

    Remove Replot, update todo

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
-import http from 'http'
-import { logger } from 'utils/Logger'
 import 'dotenv/config'
 
-const Replit = (process.env.REPLIT_DB_URL !== undefined)
 
-function initialize(replit = false) {
-  if (replit) {
-    logger.info('[REPLIT ENVIRONMENT DETECTED] [STARTING WEBSERVER]')
-
-    http.createServer((req, res) => {
-      const now = new Date().toLocaleString('en-US')
-      res.end(`OK (200) - ${now}`)
-    }).listen(3000)
-
-    return require('./index')
-  }
-
+function initialize() {
   return require('./index')
 }
 
-initialize(Replit)
+initialize()
